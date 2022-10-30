7fc9c5c974b009dfd52e695fea4a3905e47c004c
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-10-30 13:05:10 +0530

    made cli interface

--- civet/src/main.rs
+++ civet/src/main.rs
@@ -1,3 +1,27 @@
+use clap::{arg, Command};
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli::cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("join", sub_matches)) => {
+            println!(
+                "Joining {}",
+                sub_matches.get_one::<String>("CHANNEL").expect("required")
+            );
+        }
+        Some(("start", sub_matches)) => {
+            println!(
+                "Starting on {}",
+                sub_matches.get_one::<String>("CHANNEL").expect("required")
+            );
+        }
+        Some(("list", _sub_matches)) => {
+            println!(
+                "Channels are :",
+            );
+        }
+        _ => unreachable!(), 
+    }
 }
